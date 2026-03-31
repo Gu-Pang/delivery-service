d45b892dd40107bf533d37e781cc984c88357822
sionkim0126 <kimsion000@naver.com>
2026-03-31 20:21:23 +0900

    [feat]배송엔티티 생성 (#1) (#2)
    * feat : DeliveryStatus정의 및 Delivery Entity 개발
    
    * chore : 공통모듈의존성 문제 해결
    - 모든서비스 Redis의존성 강제되어 의존성 추가
    
    * feat : 공통모듈 BaseEntity 상속
    
    * fix : softDelete 검색 조건 추가
    - ErrorCode 적용
    - create deliveryDeadline 매개변수 추가

---
+++ src/main/java/org/gupang/deliveryservice/domain/entity/Delivery.java
@@ -0,0 +1,96 @@
+package org.gupang.deliveryservice.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.gupang.common.entity.BaseEntity;
+import org.gupang.common.exception.CustomException;
+import org.gupang.common.exception.ErrorCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "p_delivery")
+@Getter
+@SQLDelete(sql = "UPDATE p_delivery SET deleted_at = now() WHERE delivery_id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@NoArgsConstructor
+public class Delivery extends BaseEntity {
+
+    @Id
+    @Column(name="delivery_id")//yaml로 스네이크로 변경가능하다. 라고 한 거 같은데....일단 이렇게 설정
+    private UUID deliveryId;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @Column(name = "start_hub_id", nullable = false)
+    private UUID startHubId;
+
+    @Column(name = "end_hub_id", nullable = false)
+    private UUID endHubId;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    @Column(name = "recipient_name", nullable = false)
+    private String recipientName;
+
+    @Column(name = "delivery_manager_id")
+    private UUID deliveryManagerId;
+
+    @Column(name = "delivery_deadline")//발송시한
+    private LocalDateTime deliveryDeadline;
+
+
+    public void start() {
+        if (this.status != DeliveryStatus.READY) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+        this.status = DeliveryStatus.IN_TRANSIT;
+    }
+
+    public void complete() {
+        if( this.status != DeliveryStatus.IN_TRANSIT){
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+        this.status = DeliveryStatus.DELIVERED;
+    }
+
+    public void cancel() {
+        if (this.status == DeliveryStatus.DELIVERED) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+        this.status = DeliveryStatus.CANCELLED;
+    }
+
+    public static Delivery create(
+            UUID orderId,
+            UUID startHubId,
+            UUID endHubId,
+            String address,
+            String addressDetail,
+            String recipientName,
+            LocalDateTime deliveryDeadline
+    ) {
+        Delivery delivery = new Delivery();
+        delivery.deliveryId = UUID.randomUUID();
+        delivery.orderId = orderId;
+        delivery.startHubId = startHubId;
+        delivery.endHubId = endHubId;
+        delivery.address = address;
+        delivery.addressDetail = addressDetail;
+        delivery.recipientName = recipientName;
+        delivery.status = DeliveryStatus.READY;
+        delivery.deliveryDeadline = deliveryDeadline;
+        return delivery;
+    }
+}
---
+++ src/main/java/org/gupang/deliveryservice/domain/entity/DeliveryStatus.java
@@ -0,0 +1,9 @@
+package org.gupang.deliveryservice.domain.entity;
+
+public enum DeliveryStatus {
+    READY, //배송준비
+    IN_TRANSIT, //배송중
+    DELIVERED, //배송완료
+    CANCELLED// 배송취소
+
+}
