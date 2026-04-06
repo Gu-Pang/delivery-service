ba8200c2a6e486b45f1c6992511b6eabed7de3e9
sionkim0126 <kimsion000@naver.com>
2026-04-06 18:33:42 +0900

    feat : Repository fetch join 추가

--- src/main/java/org/gupang/deliveryservice/domain/repository/DeliveryRepository.java
+++ src/main/java/org/gupang/deliveryservice/domain/repository/DeliveryRepository.java
@@ -1,6 +1,7 @@
 package org.gupang.deliveryservice.domain.repository;
 
 import org.gupang.deliveryservice.domain.entity.Delivery;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +12,6 @@ public interface DeliveryRepository  {
     Optional<Delivery> findById(UUID uuid);
 
     Optional<Delivery> findByRoutes_RouteRecordId(UUID routeId);
+
+    Optional<Delivery> findWithRoutes(UUID deliveryId);
 }
