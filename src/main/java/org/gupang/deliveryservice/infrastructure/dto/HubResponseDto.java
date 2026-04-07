6659bb053a80b1372b71a0ff99513f709d31e901
ddangme <ssilver121@naver.com>
2026-04-08 03:02:06 +0900

    fix: 도메인 통신을 위한 json properties 설정

--- src/main/java/org/gupang/deliveryservice/infrastructure/dto/HubResponseDto.java
+++ src/main/java/org/gupang/deliveryservice/infrastructure/dto/HubResponseDto.java
@@ -1,13 +1,21 @@
 package org.gupang.deliveryservice.infrastructure.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public record HubResponseDto(
+        @JsonProperty(value = "start_hub_id")
         UUID startHubId,
+        @JsonProperty(value = "start_hub_name")
         String startHubName,
+        @JsonProperty(value = "end_hub_id")
         UUID endHubId,
+        @JsonProperty(value = "end_hub_name")
         String endHubName,
+        @JsonProperty(value = "estimated_distance")
         int estimatedDistance,
+        @JsonProperty(value = "estimated_duration")
         int estimatedDuration
 ) {
 }
